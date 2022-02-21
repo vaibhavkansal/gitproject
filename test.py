3bc2fec61ee4167cd6f768d3b92f22a36b0dff01
vaibhavkansal <41268465+vaibhavkansal@users.noreply.github.com>
2022-02-21 13:50:25 +0530

    First commit

---
+++ test.py
@@ -0,0 +1,28 @@
+import tkinter
+from tkinter import *
+
+root = Tk()
+
+L = Label(root, text="Right-click to display menu",
+          width=40, height=20)
+L.pack()
+
+m = Menu(root, tearoff=0)
+m.add_command(label="Cut")
+m.add_command(label="Copy")
+m.add_command(label="Paste")
+m.add_command(label="Reload")
+m.add_separator()
+m.add_command(label="Rename")
+
+
+def do_popup(event):
+    try:
+        m.tk_popup(event.x, event.y)
+    finally:
+        m.grab_release()
+
+
+L.bind("<Button-2>", do_popup)
+
+mainloop()
